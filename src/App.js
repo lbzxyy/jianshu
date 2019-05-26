a949400691c45cfa50eed90ad1b9b5d28fe2bfe9
leepoo <leepoo2008@163.com>
2019-05-26 12:57:59 +0800

    使用style-components来编写样式

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import Header from './common/header';
+import { GlobalStyle } from './style';
 
 class App extends Component {
     render() {
-        return <div>hello world</div>
+        return (
+            <div>
+                <GlobalStyle/>
+                <Header></Header>
+            </div>
+            
+        )
     }
 }
 
---
+++ src/common/header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+    HeaderWrapper,
+    Logo
+} from './style';
+class Header extends Component {
+    render() {
+        return (
+            <HeaderWrapper>
+                <Logo ></Logo>
+            </HeaderWrapper>
+        )
+    }
+}
+
+export default Header
