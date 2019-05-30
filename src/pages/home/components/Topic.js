224922db3e7de1b50cfd4eb11188b41980cf0ba8
李步钻 <leepoo2008@163.com>
2019-05-30 19:59:07 +0800

    home模块下添加 store管理数据
    Signed-off-by: 李步钻 <leepoo2008@163.com>

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { TopicWrapper, TopicItem } from '../style';
+import { connect } from 'react-redux';
 
 class Topic extends Component {
     render() {
+        const { topicList } = this.props;
         return(
-            <div>Topic</div>
+            <TopicWrapper>
+                {
+                    topicList.map( v => {
+                        return(
+                            <TopicItem key={v.get('id')}>
+                                <img className='topic-pic' src={v.get('imgUrl')}/>
+                                {v.get('title')}
+                             </TopicItem>
+                        )
+                    })
+                }
+               
+            </TopicWrapper>
         )
     }
 }
-export default Topic;
+const mapState = (state) => ({
+    topicList: state.getIn(['home','topicList'])
+})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+export default connect(mapState,null)(Topic);
